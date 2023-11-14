c38ae9fc1f85da7ee09caa5756012810e53c34eb
Steven <stevccru@icloud.com>
2023-11-14 11:34:26 -0500

    added Gold class

--- src/main/java/com/pluralsightfinance/Gold.java
+++ src/main/java/com/pluralsightfinance/Gold.java
@@ -1,6 +1,15 @@
 package com.pluralsightfinance;
 
-public class Gold {
+public class Gold extends FixedAsset {
 
     private double weight;
+
+    public Gold(String name, double marketValue) {
+        super(name, marketValue);
+    }
+
+    @Override
+    public double getValue() {
+        return super.getValue();
+    }
 }
